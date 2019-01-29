2c43dcb9c87add8465740fa092071eb33f770769
Anson <all197@ucsd.edu>
2019-01-29 00:27:08 -0800

    simple browser done for utube

--- Crawler2.js
+++ Crawler2.js
@@ -15,22 +15,49 @@ scrape(options).then((result) => {});
 // or with callback
 //scrape(options, (error, result) => {});
 */
-
-
-
-var fs = require('fs');
+var urlExists = require('url-exists');
+var express = require('express');
+var bodyParser = require('body-parser');
 var youtubedl = require('youtube-dl');
-var video = youtubedl('https://www.youtube.com/watch?v=ApXoWvfEYVU&start_radio=1&list=RDApXoWvfEYVU',
-  // Optional arguments passed to youtube-dl.
-  ['--format=18'],
-  // Additional options can be given for calling `child_process.execFile()`.
-  { cwd: __dirname });
- 
-// Will be called when the download starts.
-video.on('info', function(info) {
-  console.log('Download started');
-  console.log('filename: ' + info.filename);
-  console.log('size: ' + info.size);
+var fs = require('fs');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/youtube*', function(req, res) {
+
+    console.log(req.url);
+
+    let url = req.url.substring(req.url.indexOf('?url=')+5);
+
+    urlExists(url, function(err, exists) {
+        if ( exists ) {
+            // CHECK URL FOR YOUTUBE.COM/___
+            
+            var video = youtubedl(url,
+                // Optional arguments passed to youtube-dl.
+                ['--format=18'],
+                // Additional options can be given for calling `child_process.execFile()`.
+                { cwd: __dirname }
+            );
+        
+            // Will be called when the download starts.
+            video.on('info', function(info) {
+                console.log('Download started');
+                console.log('filename: ' + info.filename);
+                console.log('size: ' + info.size);
+            });
+            
+            video.pipe(fs.createWriteStream('myvideo.mp4'));
+        
+            res.send(url);
+        }
+        else {
+            res.send("invalid url");
+        }
+    });
 });
- 
-video.pipe(fs.createWriteStream('myvideo.mp4'));
+  
+app.listen(3000);
+  
+console.log("listening on port 3000");
