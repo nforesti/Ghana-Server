ab43ed186be83b98d0e3265f2c0576baf7b00d6d
Anson <all197@ucsd.edu>
2019-06-04 19:08:35 -0700

    updated subjects

--- downloader/downloadServer.js
+++ downloader/downloadServer.js
@@ -131,11 +131,12 @@ setInterval(() => {
       }
 
       queueDownload = JSON.parse(data);
-      queueDownload["complete"] = "Downoading"
 
       if ( Object.keys(queueDownload).length !== 0 ) {
         const key = Object.keys(queueDownload)[0];
         currDownload = queueDownload[key];
+
+        currDownload["complete"] = "Downloading"
 
         // Determine download type
         if ( queueDownload[key].url.indexOf("youtube") === -1 ) {
